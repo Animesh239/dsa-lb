bb4d5060ca03489edbe69266557f68a376c2743b
Animesh239 <animeshbarik239@gmail.com>
2023-12-06 00:34:45 +0530

    reversing an array

---
+++ reversearr.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+void reverse(int arr[], int n){
+    int start = 0;
+    int end = n-1;
+
+    while(start<end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
+    }
+}
+
+int main(){
+    int n;
+    cout<<"Enter the length of the array : ";
+    cin>>n;
+
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    reverse(arr,n);
+
+    cout<<"Reversed array is : ";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+    return 0;
+}
