fb50ac064fef34659cc1977f9d2e13f45d42de7e
Animesh239 <animeshbarik239@gmail.com>
2023-12-05 20:19:44 +0530

    Refactor combination logic and add factorial function

--- combinator.cpp
+++ combinator.cpp
@@ -1,35 +1,37 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-// function for combination logic
+// function to calculate factorial
+int factorial(int num)
+{
+    int fact = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        fact *= i;
+    }
+    return fact;
+}
 
-int combination(int n, int r){
+int combination(int n, int r)
+{
     // write code to find nCr
-    int factn = 1, factr = 1, factnr = 1;
-    for(int i = 1; i <= n; i++){
-        factn *= i;
-    }
-    for(int i = 1; i <= r; i++){
-        factr *= i;
-    }
-    for(int i = 1; i <= n-r; i++){
-        factnr *= i;
-    }
-    return factn/(factr*factnr);
+    int factn = factorial(n);
+    int factr = factorial(r);
+    int factnr = factorial(n - r);
+    return factn / (factr * factnr);
 }
 
 // code to find nCr using functions
 
-int main(){
+int main()
+{
     int n, r;
-    cout<<"Enter n and r : ";
-    cin>>n>>r;
+    cout << "Enter n and r: ";
+    cin >> n >> r;
 
     int result = combination(n, r);
 
-    cout<<n<<"C"<<r<<" = "<<result<<endl;
+    cout << n << "C" << r << " = " << result << endl;
 
     return 0;
 }
-
-
