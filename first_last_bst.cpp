9effabdfb2f2e935ae91394a6c486a39b8f10eb9
Animesh239 <animeshbarik239@gmail.com>
2023-12-07 17:06:55 +0530

    first and last occurances using binary search

---
+++ first_last_bst.cpp
@@ -0,0 +1,59 @@
+// In a sorted arr , u have to find the index of the fisrt and last occurence of a given k element.
+
+#include <iostream>
+using namespace std;
+
+int firstLastOcc(int arr[], int n, int k){
+    int start = 0;
+    int end = n - 1;
+    int first = -1;
+    int last = -1;
+
+    while(start <= end){
+        int mid = start + (end - start)/2;
+        if(arr[mid] == k){
+            first = mid;
+            end = mid - 1;
+        }
+        else if(arr[mid] > k){
+            end = mid - 1;
+        }
+        else{
+            start = mid + 1;
+        }
+    }
+
+    start = 0;
+    end = n - 1;
+
+    while(start <= end){
+        int mid = start + (end - start)/2;
+        if(arr[mid] == k){
+            last = mid;
+            start = mid + 1;
+        }
+        else if(arr[mid] > k){
+            end = mid - 1;
+        }
+        else{
+            start = mid + 1;
+        }
+    }
+
+    cout << first << " " << last << endl;
+    return 0;
+}
+
+void printAns(int arr[], int n, int k){
+    firstLastOcc(arr, n, k);
+}
+
+int main(){
+   int arr[] = {0,0,0,1,1,1,1,2,2,2,3,3,3,3,4};
+   int key = 3;
+   int n = sizeof(arr)/sizeof(int);
+
+   printAns(arr, n, key);
+
+   return 0;
+}
