7f408366c96242f80334dde801d5460a9a750fbb
Animesh239 <animeshbarik239@gmail.com>
2023-12-09 02:43:54 +0530

    aggressive cows

--- aggressiveCows.cpp
+++ aggressiveCows.cpp
@@ -1,51 +1,62 @@
 // There is an unsorted arr of N elements each representing the position of a stall. There are K no. of cows that need to be placed in the stalls. Find the largest minimum distance between the cows.
 
-#include<iostream>
-using namespace std ;
+#include <iostream>
+using namespace std;
 
-bool isPossible(int arr[] , int n , int k , int mid){
-    int cows = 1 ;
-    int pos = arr[0] ;
-
-    for(int i=0 ; i < n ; i++){
-        if(arr[i] - pos >= mid){
-            cows++;
+bool isPossible(int arr[], int n, int k, int mid)
+{
+    int cowCount = 1;
+    int pos = arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] - pos >= mid)
+        {
+            cowCount++;
             pos = arr[i];
-        }
-        if(cows == k){
-            return true;
+            if (cowCount == k)
+            {
+                return true;
+            }
         }
     }
     return false;
 }
 
-int findLargestMinDistance(int arr[] , int n , int k){
-    int s = 0 ;
-    int maxi = 0 ;
-    for(int i=0 ; i < n ; i++){
-        maxi = max(maxi , arr[i]) ;
+int findLargestMinDistance(int arr[], int n, int k)
+{
+    int s = 0;
+    int maxi = -1;
+    for (int i = 0; i < n; i++)
+    {
+        maxi = max(maxi, arr[i]);
     }
-    int e = maxi ;
-    int ans = -1 ;
-    int mid = s + (e - s) / 2 ;
+    int e = maxi;
 
-    while(s<=e){
-        mid = s + (e - s) / 2 ;
-        if(isPossible(arr, n , k , mid)){
-            ans = mid ;
-            s = mid + 1 ;
+    int mid = s + (e - s) / 2;
+
+    int ans = -1;
+
+    while (s <= e)
+    {
+        if (isPossible(arr, n, k, mid))
+        {
+            s = mid + 1;
+            ans = mid;
         }
-        else{
-            e = mid - 1 ;
+        else
+        {
+            e = mid - 1;
         }
+        mid = s + (e - s) / 2;
     }
-    return ans ;
+    return ans;
 }
 
-int main(){
-    int arr[6] = {0,3,4,7,10,9} ;
-    int n = 6 ;
-    int k = 4 ;
+int main()
+{
+    int arr[6] = {0, 3, 4, 7, 10, 9};
+    int n = 6;
+    int k = 4;
 
-    cout << findLargestMinDistance(arr, n , k) << endl;
+    cout << findLargestMinDistance(arr, n, k) << endl;
 }
