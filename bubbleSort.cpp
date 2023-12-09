ee12a54e662fc6980fee15cc1a73917a3c7d163d
Animesh239 <animeshbarik239@gmail.com>
2023-12-09 13:07:18 +0530

    bubble sort optimization

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,7 +1,7 @@
 // bubble sort with given arr , n ;
 
-#include<iostream>
-using namespace std ;
+#include <iostream>
+using namespace std;
 
 // void bubbleSort(int arr[] , int n){
 //     for(int i=0; i< n; i++){
@@ -13,17 +13,28 @@ using namespace std ;
 //     }
 // }
 
-void bubbleSort(int arr[] , int n){
-    for(int i=1; i< n ; i++){
-        for(int j = 0 ; j <n-i ; j++){
-            if(arr[j+1] < arr[j]){
-                swap(arr[j], arr[j+1]) ;
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        bool swapped = false;
+        for (int j = 0; j < n - i; j++)
+        {
+            if (arr[j + 1] < arr[j])
+            {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
             }
+        }
+        if (swapped == false) // if no swapping is done in the previous iteration , then the array is already sorted . It is done to optimize the code .
+        {
+            break;
         }
     }
 }
 
-int main(){
+int main()
+{
     int arr[6] = {0, 15, 1, 7, 10, 9};
     int n = 6;
 
