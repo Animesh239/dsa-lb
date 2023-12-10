9da799e068ffc2a3f247c4015855cdc31c174ee9
Animesh239 <animeshbarik239@gmail.com>
2023-12-10 11:30:43 +0530

    reverse an array after mth position

---
+++ reverse_m.cpp
@@ -0,0 +1,28 @@
+// an array arr and a position m. Reverse the array after that position.
+
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+void reverse_m(vector<int> &arr, int m){
+    reverse(arr.begin()+m,arr.end());
+}
+
+int main(){
+
+    vector<int> arr = {1,2,3,4,5,6,7,8,9,10};
+
+    int m = 5;
+
+    reverse_m(arr,m);
+
+    cout<<"Reversed array is : ";
+    for(int i=0;i<arr.size();i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+    return 0;
+
+}
