31eab8700f1ebd3454314df095342b11ea551f8f
Animesh239 <animeshbarik239@gmail.com>
2023-12-12 01:59:31 +0530

    wave print in a 2D array

---
+++ dataTypes/wavePrint.cpp
@@ -0,0 +1,23 @@
+// print in a wave form 
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> wavePrint(vector<vector<int>> arr , int nRows , int mCols){
+    vector<int> ans;
+    for(int j = 0 ; j < mCols ; j++){
+        if(j % 2 == 0){
+            for(int i = 0 ; i < nRows ; i++){
+                ans.push_back(arr[i][j]);
+            }
+        }
+        else{
+            for(int i = nRows - 1 ; i >= 0 ; i--){
+                ans.push_back(arr[i][j]);
+            }
+        }
+    }
+    return ans;
+}
+
