455bd284eb87a781f73d6c8bd6946caa1fa9c0e7
Animesh239 <animeshbarik239@gmail.com>
2023-12-12 00:22:18 +0530

    row wise sum in a 2D array

---
+++ dataTypes/rowSum2dArr.cpp
@@ -0,0 +1,23 @@
+// give the row wise sum of a 2d array
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    int arr[n][m];
+    for(int i = 0 ; i < n ; i++){
+        for(int j = 0 ; j < m ; j++){
+            cin >> arr[i][j];
+        }
+    }
+    for(int i = 0 ; i < n ; i++){
+        int sum = 0;
+        for(int j = 0 ; j < m ; j++){
+            sum += arr[i][j];
+        }
+        cout << sum << endl;
+    }
+    return 0;
+}
