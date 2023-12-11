86b9fe180c80d708f3f2d47979ae1f9f68c68b6c
Animesh239 <animeshbarik239@gmail.com>
2023-12-12 02:30:57 +0530

    spiral printing of a 2D matrix

--- dataTypes/spiralprint.cpp
+++ dataTypes/spiralprint.cpp
@@ -4,7 +4,10 @@
 #include <vector>
 using namespace std;
 
-vector<int> spiralPrint(vector<vector<int>> arr, int nRows, int mCols){
+vector<int> spiralPrint(vector<vector<int>> matrix){
+    int nRows = matrix.size();
+    int mCols = matrix[0].size();
+
     vector<int> ans;
     int startRow = 0;
     int endRow = nRows - 1;
@@ -13,25 +16,25 @@ vector<int> spiralPrint(vector<vector<int>> arr, int nRows, int mCols){
     while(startRow <= endRow && startCol <= endCol){
         // print start row
         for(int j = startCol ; j <= endCol ; j++){
-            ans.push_back(arr[startRow][j]);
+            ans.push_back(matrix[startRow][j]);
         }
         startRow++;
         // print end col
         for(int i = startRow ; i <= endRow ; i++){
-            ans.push_back(arr[i][endCol]);
+            ans.push_back(matrix[i][endCol]);
         }
         endCol--;
         // print end row
         if(endRow >= startRow){
             for(int j = endCol ; j >= startCol ; j--){
-                ans.push_back(arr[endRow][j]);
+                ans.push_back(matrix[endRow][j]);
             }
             endRow--;
         }
         // print start col
         if(endCol >= startCol){
             for(int i = endRow ; i >= startRow ; i--){
-                ans.push_back(arr[i][startCol]);
+                ans.push_back(matrix[i][startCol]);
             }
             startCol++;
         }
@@ -42,13 +45,13 @@ vector<int> spiralPrint(vector<vector<int>> arr, int nRows, int mCols){
 int main(){
     int nRows, mCols;
     cin >> nRows >> mCols;
-    vector<vector<int>> arr(nRows, vector<int>(mCols));
+    vector<vector<int>> matrix(nRows, vector<int>(mCols));
     for(int i = 0 ; i < nRows ; i++){
         for(int j = 0 ; j < mCols ; j++){
-            cin >> arr[i][j];
+            cin >> matrix[i][j];
         }
     }
-    vector<int> ans = spiralPrint(arr, nRows, mCols);
+    vector<int> ans = spiralPrint(matrix);
     for(int i = 0 ; i < ans.size() ; i++){
         cout << ans[i] << " ";
     }
