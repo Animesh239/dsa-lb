7cd015270b554be07d6c77e3bda78ec0cbeae9d8
Animesh239 <animeshbarik239@gmail.com>
2023-12-12 01:48:13 +0530

    row operations in a 2D array

--- dataTypes/rowSum2dArr.cpp
+++ dataTypes/rowSum2dArr.cpp
@@ -1,23 +1,51 @@
 // give the row wise sum of a 2d array
 
+// also give the index of largest row sum and its value
+
 #include <iostream>
+#include <climits>
 using namespace std;
 
+// row wise sum
+void rowSum(int arr[][100], int row, int column){
+    for(int i = 0 ; i < row ; i++){
+        int sum = 0;
+        for(int j = 0 ; j < column ; j++){
+            sum += arr[i][j];
+        }
+        cout << sum << " ";
+    }
+}
+
+// index of largest row sum
+void largestRowSum(int arr[][100], int row, int column){
+    int maxSum = INT_MIN;
+    int maxIndex = 0;
+    for(int i = 0 ; i < row ; i++){
+        int sum = 0;
+        for(int j = 0 ; j < column ; j++){
+            sum += arr[i][j];
+        }
+        if(sum > maxSum){
+            maxSum = sum;
+            maxIndex = i;
+        }
+    }
+    cout << maxIndex << " " << maxSum << endl;
+}
+
 int main(){
-    int n, m;
-    cin >> n >> m;
-    int arr[n][m];
-    for(int i = 0 ; i < n ; i++){
-        for(int j = 0 ; j < m ; j++){
+    
+    int arr[100][100];
+    int row, column;
+    cin >> row >> column;
+    for(int i = 0 ; i < row ; i++){
+        for(int j = 0 ; j < column ; j++){
             cin >> arr[i][j];
         }
     }
-    for(int i = 0 ; i < n ; i++){
-        int sum = 0;
-        for(int j = 0 ; j < m ; j++){
-            sum += arr[i][j];
-        }
-        cout << sum << endl;
-    }
+    rowSum(arr, row, column);
+    cout << endl;
+    largestRowSum(arr, row, column);
     return 0;
 }
