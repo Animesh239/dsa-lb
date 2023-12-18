439501ff383853ef451fe27750eea9a3fce6a7e2
Animesh239 <animeshbarik239@gmail.com>
2023-12-19 00:06:00 +0530

    jaggered arrays

---
+++ pointers/jaggeredArray.cpp
@@ -0,0 +1,41 @@
+// jaggered array
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int m ;
+    cin >> m; // number of rows
+
+    int sizes[3] = {2, 3, 4}; // number of columns in each row
+
+    int **arr = new int *[m]; 
+
+    for(int i = 0; i < m; i++){
+        arr[i] = new int[sizes[i]];
+    }
+
+    // input
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < sizes[i]; j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    // output
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < sizes[i]; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    // delete the array
+
+    for(int i = 0; i < m; i++){
+        delete [] arr[i];
+    }
+
+    return 0;
+
+}
