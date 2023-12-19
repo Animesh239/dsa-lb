2f160edd1fba08542f61542f1d67b34aeefed0e2
Animesh239 <animeshbarik239@gmail.com>
2023-12-19 22:43:02 +0530

    ining

--- recursion/sumArray.cpp
+++ recursion/sumArray.cpp
@@ -7,7 +7,8 @@ int sum( int *arr , int n){
     if(n == 0){
         return 0 ;
     }
-    return arr[0] + sum(arr + 1 , n - 1) ;
+    int smallAns = sum(arr + 1 , n - 1) ;
+    return arr[0] + smallAns ;
 }
 
 int main(){
