955d8c7b568ee873856d7572c31cd66b60665eee
Animesh239 <animeshbarik239@gmail.com>
2023-12-25 16:28:50 +0530

    permutation of an array using backtracking and recursion

---
+++ recursion/permutationStr.cpp
@@ -0,0 +1,27 @@
+// permutation of an array 
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(vector<vector<int>>& ans, vector<int>& nums, int i) {
+   // base case
+    if(i>=nums.size()) {
+         ans.push_back(nums);
+         return;
+    }
+
+    for(int j=i; j<nums.size(); j++) {
+        swap(nums[i], nums[j]); // include
+        solve(ans, nums, i+1); // recursive call
+        swap(nums[i], nums[j]); // backtrack
+    }
+}
+
+vector<vector<int>> permute(vector<int>& nums) {
+    vector<vector<int>> ans;
+    if(nums.size()==0) {return ans;}
+    int i = 0;  // starting index
+    solve(ans, nums, i);
+    return ans;
+}
